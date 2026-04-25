cca478fd2c8020960379f0990d6655526d010982
Asiq <273601675+1930849-Asiq@users.noreply.github.com>
2026-04-25 23:42:11 +0600

    add

--- lib/models/product_data_model.dart
+++ lib/models/product_data_model.dart
@@ -6,4 +6,36 @@ import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+List<ProductDataModel> productDataModelFromJson(String str) => List<ProductDataModel>.from(json.decode(str).map((x) => ProductDataModel.fromJson(x)));
+
+String productDataModelToJson(List<ProductDataModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class ProductDataModel {
+  String productId;
+  String name;
+  double price;
+  String category;
+  String imageUrl;
+  String description;
+
+  ProductDataModel({
+    required this.productId,
+    required this.name,
+    required this.price,
+    required this.category,
+    required this.imageUrl,
+    required this.description,
+  });
+
+  factory ProductDataModel.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return ProductDataModel(
+      productId: data["productId"], //?? doc.id, // Grab the unique Firestore ID: doc.id
+      name: data["name"] ?? 'Unknown Name',
+      price: (data["price"] ?? 0).toDouble(), // Safely handles ints/doubles
+      category: data["category"] ?? 'Uncategorized',
+      imageUrl: data["imageUrl"] ?? '',
+      description: data["description"] ?? '',
+    );
+  }
 
